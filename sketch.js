b2fb74e00cc73e42198e7f71cef755e9f7323b18
AartiMachha <77327410+AartiMachha@users.noreply.github.com>
2021-05-18 13:32:31 +0530

    Add files via upload
    Sunrise and Sunset game will show you the time zones of different countries using a unique animation created by writing code. 
    Go ahead and see it is amazing!!

---
+++ sketch.js
@@ -0,0 +1,92 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg="sunrise1.png" ;
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+    if(backgroundImg){
+    background(backgroundImg);
+    }
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response= await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+
+    //change the data in JSON format
+    var responseJSON= await response.json();
+
+    // write code slice the datetime
+    var datetime= responseJSON.datetime
+    var hour= datetime.slice(11,13);
+
+    console.log(hour);
+
+
+    // add conditions to change the background images from sunrise to sunset
+    if(hour>=04&&hour<=06){
+    bg="sunrise1.png"
+    }
+    else if(hour>=06&&hour<=08){
+    bg="surise2.png"
+    }
+    else if(hour>=08&&hour<=10){
+    bg="surise3.png"
+    }
+    else if(hour>=10&&hour<=12){
+    bg="surise4.png"
+    }
+    else if(hour>=12&&hour<=14){
+    bg="surise5.png"
+    }
+    else if(hour>=14&&hour<=16){
+    bg="surise6.png"
+    }
+    else if(hour>=16&&hour<=18){
+    bg="surise7.png"
+    }
+    else if(hour>=18&&hour<=20){
+    bg="surise8.png"
+    }
+    else if(hour>=20&&hour<=22){
+    bg="surise9.png"
+    }
+    else if(hour>=22&&hour<=24){
+    bg="surise10.png"
+    }
+    else if(hour>=24&&hour<=02){
+    bg="surise11.png"
+    }
+    else if(hour>=02&&hour<=04){
+    bg="surise12.png"
+    }
+    
+    //load the image in backgroundImg variable here
+    backgroundImg= loadImage(bg)
+    console.log(backgroundImg);
+}
